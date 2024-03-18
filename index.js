ff0f4f59c25f7f208f649b2057d0f982bc41c6f1
Rahul Kumar <rahulkumarpahwa@gmail.com>
2024-03-18 21:00:54 +0530

    created the form page and post route

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.use(express.json()); // for json
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/register", (req, res) => {
+app.get("/form", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  // res.send("Got the post request");
+  res.send(req.body);
 });
 
 app.listen(port, () => {
