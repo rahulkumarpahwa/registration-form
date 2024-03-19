2ecfcf86364fd92af9490494193b6dc44e24b9b8
Rahul Kumar <rahulkumarpahwa@gmail.com>
2024-03-19 23:56:39 +0530

    created the schema and linked the atlasdb

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Register = require("./models/register");
+require("dotenv").config();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json()); // for json
 app.use(express.urlencoded({ extended: true }));
 
+async function main() {
+  await mongoose.connect(process.env.ATLASDB_URL);
+}
+
+main()
+  .then((res) => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("/form");
 });
 
 app.get("/form", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  // res.send("Got the post request");
-  res.send(req.body);
+app.post("/register", async (req, res) => {
+  try {
+    // console.log(req.body);
+    // res.send("Got the post request");
+    const { name, email, password } = req.body;
+    let newRegister = new Register({
+      name: name,
+      email: email,
+      password: password,
+    });
+    await newRegister.save();
+    // res.render("/");
+    res.send("user Registered successfully");
+  } catch (error) {
+    console.log("Error" + error);
+  }
 });
 
 app.listen(port, () => {
---
+++ models/register.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const registerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Register = mongoose.model("Register", registerSchema);
+
+module.exports = Register;
